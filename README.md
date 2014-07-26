3ed10d6f64540dbff92c783becafb303efd7b544
Garry Yao <yaojun85@gmail.com>
2014-07-26 15:24:27 +0800

    the first commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# syncme [![Build Status](https://secure.travis-ci.org/garryyao/syncme.png?branch=master)](http://travis-ci.org/garryyao/syncme)
+
+> Ease syncing files in between two machines with Rsync
+
+
+## Getting Started
+
+Install the module with: `npm install syncme`
+
+```js
+var syncme = require('syncme');
+syncme.awesome(); // "awesome"
+```
+
+Install with cli command
+
+```sh
+$ npm install -g syncme
+$ syncme --help
+$ syncme --version
+```
+
+
+```sh
+# creates a browser.js
+$ grunt browserify
+```
+
+
+
+## Documentation
+
+_(Coming soon)_
+
+
+## Examples
+
+_(Coming soon)_
+
+
+## Contributing
+
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com).
+
+
+## License
+
+Copyright (c) 2014 Garry Yao  
+Licensed under the MIT license.
---
+++ cli.js
@@ -0,0 +1,47 @@
+#! /usr/bin/env node
+
+'use strict';
+
+require('shelljs/global');
+
+var format = require('mustache').render;
+var findup = require('findup-sync');
+var fs = require('fs');
+var readline = require('readline');
+var path = require('path');
+var tilde = require('expand-home-dir');
+
+var userArgs = process.argv;
+var host = userArgs[2];
+
+if (userArgs.indexOf('-h') !== -1 || userArgs.indexOf('--help') !== -1 || host === undefined) {
+  return console.log('cli help');
+}
+
+if (userArgs.indexOf('-v') !== -1 || userArgs.indexOf('--version') !== -1) {
+  return console.log(require('./package').version);
+}
+
+var rd = readline.createInterface({
+  input: fs.createReadStream(findup('.rsyncrc', {nocase: true})),
+  output: process.stdout,
+  terminal: false
+});
+
+rd.on('line', function(file) {
+  file = tilde(file).replace(/\s+/g, '\\$&');
+  var cmd = format(
+    "rsync -vazu {{#exclude}} --exclude= {{exclude}}{{/exclude}} {{&src}} {{host}}:'{{&dest}}'",
+    {
+      exclude: '',
+      host: host,
+      src: file,
+      dest: path.dirname(file),
+    });
+
+  exec(cmd);
+});
+
+rd.on('close', function() {
+  process.exit(0);
+});
